1aeb374a91c7b5410d9e5bf77f616c7efc0f488e
mofthecross <mofthecross@gmail.com>
2016-11-03 13:43:20 -0700

    Add script to package.json

---
+++ server/server.js
@@ -0,0 +1,16 @@
+let webpack = require('webpack');
+let WebpackDevServer = require('webpack-dev-server');
+let config = require('../webpack.config');
+
+let server = new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+})
+
+server.listen(4000, 'localhost', function (error, result) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Listening at http://localhost:4000/');
+});
