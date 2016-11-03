2dcd0db657248e9bad8c949c095ee39f50f108a2
mofthecross <mofthecross@gmail.com>
2016-11-03 13:47:38 -0700

    add app.js file and render initial/ basic react component

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Duplicate String Checker</h1>
+      </div>
+    );
+  }
+}
