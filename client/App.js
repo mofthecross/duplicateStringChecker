3815fb8b99abb12983ebb3bd4071d519c64642a4
mofthecross <mofthecross@gmail.com>
2016-11-03 13:58:22 -0700

    Render input component

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Input from './components/Input';
 
 export default class App extends Component {
-  
   render() {
     return (
       <div>
         <h1>Duplicate String Checker</h1>
+        <Input/>
       </div>
     );
   }
