f412d0e8a2dc7e280dae47d5bd605af50f4c6c7e
mofthecross <mofthecross@gmail.com>
2016-11-03 14:17:25 -0700

    Implement handleChange event handler to change input state when user begins typing

--- client/components/Input.js
+++ client/components/Input.js
@@ -8,17 +8,30 @@ export default class Input extends Component {
         input: '',
         output: ''
       }
+      this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    console.log(event);
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+
+
   render() {
     return (
       <div>
       <div>
         <input type='text'
-               placeholder= 'type your string here'>
+               placeholder= 'type your string here'
+               value={this.state.input}
+               onChange={this.handleChange}>
        </input>
        <button>check</button>
       </div>
-        <Output entry={this.state.sendtoOutput}/>
+        <Output entry={this.state.output}/>
       </div>
     );
   }
