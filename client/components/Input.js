9323358ea50e2e6f545f8cc28b05bd2672bd8e96
mofthecross <mofthecross@gmail.com>
2016-11-03 14:05:43 -0700

    Render output component by passing prop from Input

--- client/components/Input.js
+++ client/components/Input.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Output } from './Output';
 
 export default class Input extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        input: 'sendtoOUtput'
+      }
+  }
   render() {
     return (
       <div>
         <h1>this is an input</h1>
+        <Output entry={this.state.sendtoOutput}/>
       </div>
     );
   }
